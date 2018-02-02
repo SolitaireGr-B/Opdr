63ae53766ee8523cf2c5ae2515b473fde98c58bf
Wesdewess <35997013+Wesdewess@users.noreply.github.com>
2018-02-02 14:28:57 +0100

    ehm, er is een button ofzo

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,11 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import javax.swing.*;
@@ -13,16 +15,42 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        final SwingNode swingNode = new SwingNode();
+        createAndSetSwingContent(swingNode);
+        Pane pane = new Pane();
+        pane.getChildren().add(swingNode);
+
+
+
+       // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Boter Kaas en Eieren");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(pane, 300, 300));
         primaryStage.show();
 
 
     }
-public static void openform(){
-    bke BKE = new bke();
-        BKE.setVisible(true);
+
+    private void createAndSetSwingContent(final SwingNode swingNode) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JPanel panel = new JPanel();
+                JTextArea ta = new JTextArea(100, 100);
+        panel.add(new JTextArea(""));
+        panel.add(new JTextArea(""));
+        panel.add(new JTextArea(""));
+        panel.add(new JTextArea(""));
+        panel.add(new JButton("Gutentag, press me!", ));
+                swingNode.setContent(panel);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+
     }
 
 
